4592ab72572816cbfc3fb64d3cb61bfce8033b3f
Md <iri.irfan007@gmail.com>
2024-01-07 02:13:39 +0530

    111:Create Save Post Feature Entity Class Added 4th Jan 2024

---
+++ src/main/java/com/myblog/blog1/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.myblog.blog1.repository;
+
+import com.myblog.blog1.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
---
+++ src/main/java/com/myblog/blog1/service/PostService.java
@@ -0,0 +1,8 @@
+package com.myblog.blog1.service;
+
+import com.myblog.blog1.payload.PostDto;
+
+public interface PostService {
+
+    PostDto createPost(PostDto postDto);
+}
