60bb0f963e065c3700a5066ea94c5847e474b1ac
Md <iri.irfan007@gmail.com>
2024-01-06 18:36:13 +0530

    111:Create Save Post Feature Entity Class Added

--- src/main/java/com/myblog/blog1/Blog1Application.java
+++ src/main/java/com/myblog/blog1/Blog1Application.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Blog1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Blog1Application.class, args);
+	public static void main(String[] args) {SpringApplication.run(Blog1Application.class, args);
 	}
 
 }
